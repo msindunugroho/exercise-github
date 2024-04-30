6ca14a6ac55308b7b186d4f9c5077a2e2e0405c8
sindu <113517721+msindunugroho@users.noreply.github.com>
2024-04-30 16:56:57 +0700

    Update main.js: create animationCard function

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const main = [];
+function animationCard(second, timingFunc, delay) {
+  const cards = document.querySelectorAll('.card);
+  cards.forEach(card => {
+    card.addEventListener('click', (e) => {
+      e.target.img.classList.toggle('fade');
+    })
+  })
+}
