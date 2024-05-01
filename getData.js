c6c30c17a7c435982c7b44e6abd26a7e91893858
sindu <113517721+msindunugroho@users.noreply.github.com>
2024-05-01 09:52:10 +0700

    Update getData.js: add deafault value and key, and get response to console

--- getData.js
+++ getData.js
@@ -1,7 +1,8 @@
 export const getData = (key, value) => {
-  return fetch(`https://www.google.com/global-API/?${key}=${value}`)
+  return fetch(`https://www.google.com/global-API/?${key || 'i'}=${value || 'transformers'}`)
     .then(response => {
       if(!response.ok) throw new Error(response.statusText)
+      console.log(response)
       return response
     })
   .then(response => {
